b744af76ffe7b10a111265dfb15681552a00187c
Garo <94136201+MagoGaro@users.noreply.github.com>
2024-07-06 12:52:42 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,7 +8,8 @@ async function getConnection() {
         host: process.env.HOST,
         database: process.env.DATABASE,
         user: process.env.USER,
-        password: process.env.PASSWORD
+        password: process.env.PASSWORD,
+        port: process.env.PORT
       });
   
       console.log('conectado Satisfactoriamente');
