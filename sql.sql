fd74c7b3b3e3bbd69a88835dbab74fd8f06d070b
Garo <94136201+MagoGaro@users.noreply.github.com>
2024-07-07 22:58:39 -0300

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -24,3 +24,39 @@ id int auto_increment primary key
 , usuario varchaR(100) DEFAULT(user())
 , accion varchar(20)
 );
+
+create table usuarios(
+id int auto_increment primary key
+, nombre varchar(100)
+, apellido varchar(100)
+, mail varchar(150) unique
+, pass char(100)
+, fecha_hora datetime DEFAULT(now())
+);
+
+drop trigger modif_registro
+delimiter //
+
+create trigger alta_registro
+AFTER INSERT ON productos
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_producto(nombre,precio,fecha_hora,accion) VALUES
+	(new.nombre,new.precio,now(),'Creación');
+END//
+
+create trigger modif_registro
+AFTER update ON productos
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_producto(nombre,precio,fecha_hora,accion) VALUES
+	(new.nombre,new.precio,now(),'Modificación');
+END//
+
+create trigger baja_registro
+after delete ON productos
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_producto(nombre,precio,fecha_hora,accion) VALUES
+	(old.nombre,old.precio,now(),'Eliminación');
+END//
